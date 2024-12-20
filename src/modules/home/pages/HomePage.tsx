f6d81a67889125bd04ce6c9d04ccbc118e618d8c
ys-yoqub <ys.yoqub@gmail.com>
2024-12-20 15:38:40 +0500

    added: .env config, refactored, tested: home page

--- src/modules/home/pages/HomePage.tsx
+++ src/modules/home/pages/HomePage.tsx
@@ -1,4 +1,12 @@
+import { useEffect } from "react";
+import api from "../../../services/api";
+
 const HomePage = () => {
+  useEffect(() => {
+    api.get("users").then((response) => {
+      console.log(response.data);
+    });
+  }, []);
   return (
     <div>
       <h1>Home Page</h1>
---
+++ src/services/api/index.ts
@@ -0,0 +1,30 @@
+import axios from "axios";
+const baseURL = import.meta.env.VITE_BASE_URL;
+
+const api = axios.create({
+  baseURL,
+  headers: {},
+});
+
+api.interceptors.request.use(
+  (config) => {
+    const token = localStorage.getItem("authToken");
+    if (token) {
+      config.headers["Authorization"] = `Bearer ${token}`;
+    }
+    return config;
+  },
+  (error) => Promise.reject(error)
+);
+
+api.interceptors.response.use(
+  (response) => response,
+  (error) => {
+    if (error.response && error.response.status === 401) {
+      console.log("401");
+    }
+    return Promise.reject(error);
+  }
+);
+
+export default api;
