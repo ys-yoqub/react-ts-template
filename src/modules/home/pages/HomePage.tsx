bc3f461a326081e689a51da3b42f68243c4a4477
ys-yoqub <ys.yoqub@gmail.com>
2025-01-24 17:19:21 +0500

    init

--- src/modules/home/pages/HomePage.tsx
+++ src/modules/home/pages/HomePage.tsx
@@ -1,4 +1,7 @@
 import { useFetch } from "../../../shared/hooks/useCrud";
+import { useFormik } from "formik";
+import * as Yup from "yup";
+import { TextField, Button, Box } from "@mui/material";
 
 const HomePage = () => {
   const fetchUsers = useFetch(["users"], "/users");
@@ -7,10 +10,80 @@ const HomePage = () => {
     fetchUsers.refetch();
     console.log("refetching data", fetchUsers.data);
   };
+
+  const formik = useFormik({
+    initialValues: {
+      email: "",
+      password: "",
+    },
+    validationSchema: Yup.object({
+      email: Yup.string()
+        .email("Invalid email address")
+        .required("Email is required"),
+      password: Yup.string()
+        .min(8, "Password must be at least 8 characters long")
+        .required("Password is required"),
+    }),
+    onSubmit: (values) => {
+      console.log("Form data:", values);
+      alert("Form submitted!");
+    },
+  });
+
   return (
     <div>
-      <h1>Home Page</h1>
-      <button onClick={handleRefetch}>refetch data</button>
+      <Box
+        sx={{
+          maxWidth: "400px",
+          margin: "0 auto",
+          display: "flex",
+          flexDirection: "column",
+          gap: 2,
+        }}
+      >
+        <form onSubmit={formik.handleSubmit}>
+          {/* Email Field */}
+          <TextField
+            fullWidth
+            id="email"
+            name="email"
+            label="Email"
+            variant="outlined"
+            value={formik.values.email}
+            onChange={formik.handleChange}
+            onBlur={formik.handleBlur}
+            error={formik.touched.email && Boolean(formik.errors.email)}
+            helperText={formik.touched.email && formik.errors.email}
+            sx={{ mb: 2 }}
+          />
+
+          {/* Password Field */}
+          <TextField
+            fullWidth
+            id="password"
+            name="password"
+            label="Password"
+            type="password"
+            variant="outlined"
+            value={formik.values.password}
+            onChange={formik.handleChange}
+            onBlur={formik.handleBlur}
+            error={formik.touched.password && Boolean(formik.errors.password)}
+            helperText={formik.touched.password && formik.errors.password}
+          />
+
+          {/* Submit Button */}
+          <Button
+            type="submit"
+            variant="contained"
+            color="primary"
+            fullWidth
+            sx={{ mt: 2 }}
+          >
+            Submit
+          </Button>
+        </form>
+      </Box>
     </div>
   );
 };
--- src/shared/components/Sidebar.tsx
+++ src/shared/components/Sidebar.tsx
@@ -1,7 +1,7 @@
 const Sidebar = () => {
   return (
     <div>
-      <h1>Sidebar</h1>
+      {/* <h1>Sidebar</h1> */}
     </div>
   );
 };
